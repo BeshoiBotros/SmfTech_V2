88fc57eaced842c7b2039b37e14461d751214e7b
Beshoy <78441247+BeshoiBotros@users.noreply.github.com>
2024-06-25 13:31:46 +0300

    .

--- SmfTech/settings.py
+++ SmfTech/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'SmfTech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SMF',
+        'USER' : 'smf',
+        'PASSWORD' : 'smf_tech',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306' 
     }
 }
 
